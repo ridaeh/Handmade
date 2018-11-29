a00ac97dbd3236f35466390b0f8f9d46fc799cc1
ridaeh <r7hamdan@enib.fr>
2018-11-29 03:58:39 +0100

    Fetching data from the API

--- src/client/components/offers.js
+++ src/client/components/offers.js
@@ -3,38 +3,30 @@ import {render} from 'react-dom'
 import {ArticleItem} from './article-item'
 export class Offers extends React.Component{
   constructor(props){
-        super(props)
-        this.data = [
-            { id:"213" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"214" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"215" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"216" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"217" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"218" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"219" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"},
-            { id:"210" ,name: "Hy", link: "http://www.pretacreer.com/2394-large_default/kit-diy-tapis-pompons.jpg",price:"13"}
-          ];
-
+      super(props)
+      this.state={
+        data : []
+      }
   }
   componentDidMount(){
     document.title = "Offers"
-    >console.log("FROM CLIENT HELLLLLLOOOOOOOOOOOO");
-    this.fetchData();
+    this.fetchData()
   }
   fetchData(){
-    fetch('http://localhost:3000/api/v1/articles')
-    .then(response=>response.json())
-    .then(parsedJSON=>console.log("FROM CLIENT",parsedJSON.res))
+    fetch('/api/v1/articles')
+    .then(response=> response.json())
+    .then(parsedJSON=>{this.setState({data:parsedJSON})})
     .catch(error=>console.log('parsing failed',error))
+
   }
   render(){
-    const items=this.data.map((item,index) =>
-  <ArticleItem key={index} id={item.id} name={item.name} link={item.link} price={item.price}/>);
-    return (
-      <section>
-      <h1>TODAY DEALS</h1>
-      {items}
-      </section>
-  );
+    const items=this.state.data.map((item,index) =>
+      <ArticleItem key={index} id={item._id} name={item.label} link={item.img} price={item.price}/>);
+        return (
+          <section>
+          <h1>TODAY DEALS</h1>
+          {items}
+          </section>
+      )
   }
 }
