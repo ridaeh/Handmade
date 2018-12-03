2a4f0b00db2896c115ab3cbe1f438d3b1dbd6972
yahya.fatimaezzahra@gmail.com <yahya.fatimaezzahra@gmail.com>
2018-12-03 15:04:48 +0100

    add product to Db

--- src/client/components/sellers.js
+++ src/client/components/sellers.js
@@ -1,13 +1,53 @@
-import React from "react"
+import React, { Component } from "react"
 import {render} from "react-dom"
-
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 export class Sellers extends React.Component{
-  componentDidMount(){
-    document.title = "Selles"
+
+  constructor() {
+    super();
+    this.state = {
+      label: '',
+      price: '',
+      img: ''
+    };
   }
-  render(){
+  onChange (e) {
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+
+      console.log("dones");
+  }
+
+  onSubmit (e){
+    e.preventDefault();
+
+    const {label,price,img} = this.state;
+
+    axios.post('/api/v1/sellers', {label,price,img})
+      .then((result) => {
+        this.props.history.push("/")
+      });
+        console.log("doness");
+  }
+
+  render() {
+    const {label,img,price} = this.state;
     return (
-      <h1>Sellers</h1>
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input type="text"  name="label" onChange={this.onChange.bind(this)} placeholder="label"  />
+                <input type="text"  name="img"   onChange={this.onChange.bind(this)} placeholder="img" />
+                <input type="text"  name="price"  onChange={this.onChange.bind(this)} placeholder="price"  />
+
+              <button type="submit" >Submit</button>
+            </form>
+
+
+
     );
   }
 }
+
+export default Sellers;
--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -1,6 +1,6 @@
 var express =require('express')
 var Product=require('../models/product.js')
-
+var jwt =require('jsonwebtoken')
 let productsRoutes = express.Router()
 
 productsRoutes.get('/api/v1/products', (req, res)=> {
@@ -10,6 +10,44 @@ productsRoutes.get('/api/v1/products', (req, res)=> {
         res.json(documents)
         }
     })
-  })
+  });
+
+  //api for Insert data from database
+  productsRoutes.post("/api/v1/sellers",function(req,res){
+
+      var mod = new Product(req.body);
+          mod.save(function(err,data){
+              if(err){
+                  res.send(err);
+              }
+              else{
+                console.log("record added")
+                res.status(200).json({token: jwt.sign({label: data.label,img: data.img,price: data.price, _id: data._id})})
+              }
+          });
+
+
+      /*      let product = new Product({
+              label:req.body.label,
+              price:req.body.price,
+              img: req.body.img,
+
+            });
+
+            product.save()
+              .then((result) => {
+                res.json(
+                {
+                    _id: result._id,
+                    label: result.label,
+                    price: result.price,
+                    img: result.img
+                  }
+                });
+              })*/
+
+          });
+
+
 
 module.exports = productsRoutes
