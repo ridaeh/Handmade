17488820c7ee9c693801578f29a925e95e28272d
yahya.fatimaezzahra@gmail.com <yahya.fatimaezzahra@gmail.com>
2018-12-10 16:27:28 +0100

    adding products

--- src/client/components/sellers.js
+++ src/client/components/sellers.js
@@ -1,51 +1,137 @@
 import React, { Component } from "react"
 import {render} from "react-dom"
 import axios from 'axios';
+import '../styles/login.sass'
 import { Link } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import Dropzone from 'react-dropzone'
+
+const thumbsContainer = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
+
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  marginBottom: 8,
+  marginRight: 8,
+  width: 100,
+  height: 100,
+  padding: 4,
+  boxSizing: 'border-box'
+};
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+}
+
+const image = {
+  display: 'block',
+  width: 'auto',
+  height: '100%'
+};
+
 export class Sellers extends React.Component{
 
   constructor() {
     super();
     this.state = {
-      label: '',
-      price: '',
-      img: ''
+      product:{
+        label: '',
+        price: '',
+        img : ''
+      },
+      img: []
     };
   }
-  onChange (e) {
+  onDrop(img) {
     const state = this.state
-    state[e.target.name] = e.target.value;
+    state.product['img'] =img[0];
+    state['img'] =img.map(file => Object.assign( {preview: URL.createObjectURL(file)}))
     this.setState(state);
 
-      console.log("dones");
+ }
+ componentWillUnmount() {
+  // Make sure to revoke the data uris to avoid memory leaks
+  this.state.img.forEach(f => URL.revokeObjectURL(f.preview))
+}
+
+  onChange (e) {
+    const state = this.state
+    state.product[e.target.name] = e.target.value;
+    this.setState(state);
   }
 
   onSubmit (e){
     e.preventDefault();
 
-    const {label,price,img} = this.state;
+    var bodyFormData = new FormData()
+    for (var key in this.state.product) {
+       //    if (this.state.product[key] != '')
+            bodyFormData.append(key, this.state.product[key])
+        }
+        axios.post('/api/v1/sellers/', bodyFormData).
+        then((response) => {
 
-    axios.post('/api/v1/sellers', {label,price,img})
-      .then((result) => {
-        this.props.history.push("/")
-      });
-        console.log("doness");
+      console.log(response.data)
+
+    }).catch(function(error) {
+      console.log(error);
+    });
+        console.log(this.state);
   }
 
   render() {
-    const {label,img,price} = this.state;
-    return (
+      const {label,price,img} = this.state;
+
+    const thumbs = img.map(file => (
+      <div style={thumb}>
+        <div style={thumbInner}>
+          <img
+            src={file.preview}
+            style={image}
+          />
+        </div>
+      </div>
+    ));
+
+
+    return (<section class="login">
+      <div class="columns" >
+        <div class="column"  >
             <form onSubmit={this.onSubmit.bind(this)}>
-                <input type="text"  name="label" onChange={this.onChange.bind(this)} placeholder="label"  />
-                <input type="text"  name="img"   onChange={this.onChange.bind(this)} placeholder="img" />
-                <input type="text"  name="price"  onChange={this.onChange.bind(this)} placeholder="price"  />
-
-              <button type="submit" >Submit</button>
+              <div class="field">
+                <label class="label subtitle">Product's label</label>
+                  <div class="control">
+                    <input type="text"  name="label" onChange={this.onChange.bind(this)} placeholder="label"  />
+                  </div>
+              </div>
+              <div class="field">
+                <label class="label subtitle">Price</label>
+                  <div class="control">
+                      <input type="text"  name="price"  onChange={this.onChange.bind(this)} placeholder="price"  />
+                  </div>
+              </div>
+              <div class="field">
+                <label class="label subtitle">Drop your images here </label>
+                  <div class="control">
+                  <section>
+                  <Dropzone accept="image/*"
+            onDrop={this.onDrop.bind(this)} multiple={true}>Drop your images here </Dropzone>
+            <aside style={thumbsContainer}>{thumbs}</aside>
+                  </section>
+                  </div>
+              </div>
+              <button type="submit" class="button is-large is-fullwidth" >Submit</button>
             </form>
-
-
-
+          </div>
+      </div>
+</section>
     );
   }
 }
--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -1,7 +1,24 @@
-var express =require('express')
+var express =require('express');
 var Product=require('../models/product.js')
-var jwt =require('jsonwebtoken')
-let productsRoutes = express.Router()
+const  multer = require('multer');
+let productsRoutes = express.Router();
+//const upload = multer({dest : './uploads'});
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null,file.originalname);
+  }
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+
+});
 
 productsRoutes.get('/api/v1/products', (req, res)=> {
     res.type('application/json')
@@ -13,7 +30,26 @@ productsRoutes.get('/api/v1/products', (req, res)=> {
   });
 
   //api for Insert data from database
-  productsRoutes.post("/api/v1/sellers",function(req,res){
+          productsRoutes.post("/api/v1/sellers", upload.single('img'), (req, res, next) => {
+            const product = new Product({
+
+              label: req.body.label,
+              price: req.body.price,
+              img: req.file.path
+            });
+            product.save(function(err,data){
+                if(err){
+                    res.send(err);
+                }
+                else{
+                  console.log("record added")
+                  res.status(200).json("added");
+                }
+            });
+          });
+
+  //api for Insert data from database
+/*  productsRoutes.post("/api/v1/sellers",function(req,res){
 
       var mod = new Product(req.body);
           mod.save(function(err,data){
@@ -22,32 +58,11 @@ productsRoutes.get('/api/v1/products', (req, res)=> {
               }
               else{
                 console.log("record added")
-                res.status(200).json({token: jwt.sign({label: data.label,img: data.img,price: data.price, _id: data._id})})
+                res.status(200).end();
               }
           });
-
-
-      /*      let product = new Product({
-              label:req.body.label,
-              price:req.body.price,
-              img: req.body.img,
-
-            });
-
-            product.save()
-              .then((result) => {
-                res.json(
-                {
-                    _id: result._id,
-                    label: result.label,
-                    price: result.price,
-                    img: result.img
-                  }
-                });
-              })*/
-
           });
 
-
+*/
 
 module.exports = productsRoutes
