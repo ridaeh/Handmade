3763eeec516560f7fa71686e026e28b961863be8
yahya.fatimaezzahra@gmail.com <yahya.fatimaezzahra@gmail.com>
2018-12-18 12:57:17 +0100

    add seach

--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -6,14 +6,26 @@ var productHandlers = require('../controllers/product')
 let productsRoutes = express.Router();
 
 
-productsRoutes.get('/api/v1/products', (req, res)=> {
-    res.type('application/json')
-    Product.find({}).exec(function (error, documents) {
-      if (documents.length > 0) {
-        res.json(documents)
-        }
-    })
-  });
+// productsRoutes.get('/api/v1/products', (req, res)=> {
+//     res.type('application/json')
+//     Product.find({}).exec(function (error, documents) {
+//       if (documents.length > 0) {
+//         res.json(documents)
+//         }
+//     })
+//   });
+// research bar
+  productsRoutes.get('/api/v1/products', (req, res)=> {
+      let s= req.query.search != undefined ? req.query.search : ""
+      let p=req.query.price != undefined ? req.query.price : ""
+      res.type('application/json')
+      Product. find({$and:[{label:{"$regex":s , "$options":"i"},price:{ $lte:p } }]}).exec(function (error, documents) {
+        if (documents.length > 0) {
+          res.json(documents)
+        }else
+          res.json([{}])
+      })
+    });
 
 //api for Insert data from database
 productsRoutes.route("/api/v1/product")
@@ -21,7 +33,8 @@ productsRoutes.route("/api/v1/product")
               .put(authMiddleware, imageMiddleware.upload.single('img'), productHandlers.updateProduct)
               .delete(authMiddleware,productHandlers.deleteProduct)
 
+
 productsRoutes.route("/api/v1/product/:id")
               .get(productHandlers.getProduct)
-              
+
 module.exports = productsRoutes
